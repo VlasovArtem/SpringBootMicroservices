345d86bf942957cb298195689ed63ddd72fe7c82
Vlasov Artem <avlasov@selectica.com>
2019-08-16 12:37:46 +0300

    Add Zuul Gateway service

---
+++ photoappapizuulapigateway/src/main/java/org/avlasov/photoapp/api/gateway/security/AuthorizationFilter.java
@@ -0,0 +1,55 @@
+package org.avlasov.photoapp.api.gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager, Environment environment) {
+        super(authenticationManager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (header == null || !header.startsWith(environment.getProperty("authorization.token.header.name.prefix"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken token = getAuthentication(header);
+
+        SecurityContextHolder.getContext().setAuthentication(token);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(String header) {
+        String token = header.replace(environment.getProperty("authorization.token.header.name.prefix"), "");
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+    }
+}
